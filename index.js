722e5705f0f27c98e02090e00b2157de15b6394e
violanerd <madalyne.cross@gmail.com>
2022-08-12 20:10:43 -0500

    got the call back working

--- index.js
+++ index.js
@@ -24,20 +24,41 @@ function getActivity () {
         name: 'activity',
         message: "What would you like to do?",
         choices: choices
-        }]).then(data => {
-            let activity = data.activity
-            if (activity === 'View All Employees'){
-                getEmployees();
-            }
-            if (activity === 'View All Roles'){
-                getRole();
-            }
+        }]).then(({ activity }) => {
             if (activity === 'Quit'){
-                db.end();
+                 return db.end();
+            }
+            else{
+                figureOutQuery(activity);
             }
         });
     
 }
+// this works but recursively calling getActivity at the end breaks it.
+const allEmployees = `SELECT e.id AS employee_id, e.first_name, e.last_name, role.title AS job_title, department.name AS department_name, role.salary AS salary, CASE 
+WHEN e.manager_id IS NULL THEN 'no manager' ELSE CONCAT(m.first_name,' ', m.last_name) END AS manager
+FROM employee e LEFT JOIN employee m ON m.id = e.manager_id LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id;`;
 
+const allRoles = `SELECT role.id AS role_id, role.title AS job_title, role.salary AS salary, department.name AS department FROM role JOIN department ON role.department_id = department.id;`
+
+function figureOutQuery(activity) {
+    if (activity === 'View All Employees'){
+        sql = allEmployees;
+    }
+    if (activity === 'View All Roles'){
+        sql = allRoles;
+    }
+    queryDbwithResults(sql);
+}
+
+function queryDbwithResults(sql) {
+    db.query(sql, (err, rows) => {//input selected sql
+    if (err) {
+        console.log(err.message);
+    }
+    console.table(rows);
+    getActivity();
+    })
+};
 
 getActivity();
--- queries.js
+++ queries.js
@@ -1,23 +1,33 @@
 const db = require('./db/connection');
 
-function getEmployees() {
-    db.query(`SELECT e.id AS employee_id, e.first_name, e.last_name, role.title AS job_title, department.name AS department_name, role.salary AS salary, CASE 
+// rewrite this to be sql statements
+// ONE function or one that returns and one that edits -- these ones will take parameters.
+
+const allEmployees = `SELECT e.id AS employee_id, e.first_name, e.last_name, role.title AS job_title, department.name AS department_name, role.salary AS salary, CASE 
 WHEN e.manager_id IS NULL THEN 'no manager' ELSE CONCAT(m.first_name,' ', m.last_name) END AS manager
-FROM employee e LEFT JOIN employee m ON m.id = e.manager_id LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id;`, (err, rows) => {
+FROM employee e LEFT JOIN employee m ON m.id = e.manager_id LEFT JOIN role ON e.role_id = role.id LEFT JOIN department ON role.department_id = department.id;`;
+
+const allRoles = `SELECT role.id AS role_id, role.title AS job_title, role.salary AS salary, department.name AS department FROM role JOIN department ON role.department_id = department.id;`
+
+function figureOutQuery(activity) {
+    if (activity === 'View All Employees'){
+        sql = allEmployees;
+    }
+    if (activity === 'View All Roles'){
+        sql = allRoles;
+    }
+    queryDbwithResults(sql);
+}
+
+function queryDbwithResults(sql) {
+    db.query(sql, (err, rows) => {//input selected sql
     if (err) {
         console.log(err.message);
     }
     console.table(rows);
+    //getActivity()
 })
 };
 
-function getRole() {
-    db.query(`SELECT role.id AS role_id, role.title AS job_title, role.salary AS salary, department.name AS department FROM role JOIN department ON role.department_id = department.id;`, (err, rows) => {
-        if (err) {
-            console.log(err.message);
-        }
-        console.table(rows);
-    })
-};
 
-module.exports = {getEmployees, getRole};
+
